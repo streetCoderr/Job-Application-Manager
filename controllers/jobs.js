0822e3e7e34e63efb89832a708b30a85a9e8cce0
streetCoder <streetcoder99@gmail.com>
2022-06-24 14:20:03 +0100

    implement logic for Job controller

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,27 +1,45 @@
-const { 
-  CustomError, 
-  BadRequest, 
-  Unauthorized, 
-  NotFound } = require("../errors");
+const Job = require("../models/Job")
+const { StatusCodes } = require("http-status-codes")
+const { BadRequest, NotFound } = require("../errors");
   
-const getAllJobs = (req, res) => {
-  res.json(req.user);
+const getAllJobs = async (req, res) => {
+  const createdBy = req.user.userId;
+  const jobs = await Job.find({createdBy});
+  res.status(StatusCodes.OK).json({jobs})
 }
 
-const createJob = (req, res) => {
-  res.send("create job");
+const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body)
+  res.status(StatusCodes.CREATED).json(job)
 }
 
-const getJob = (req, res) => {
-  res.send("get a single job");
+const getJob = async (req, res) => {
+  const createdBy = req.user.userId;
+  const jobID = req.params.id
+  const job = await Job.findOne({createdBy, _id: jobID})
+  if (!job) throw new NotFound(`The given id: ${jobID} has no job associated with it`)
+  res.status(StatusCodes.OK).json(job)
 }
 
-const updateJob = (req, res) => {
-  res.send("update job");
+const updateJob = async (req, res) => {
+  const createdBy = req.user.userId;
+  const jobID = req.params.id
+  const { company, position, status } = req.body;
+  if (!company || !position || !status)
+    throw new BadRequest("Please provide all necessay fields")
+  const job = await Job.findByIdAndUpdate({createdBy, _id: jobID},
+     {company,position, status}, {new: true});
+  if (!job) throw new NotFound(`The given id: ${jobID} has no job associated with it`)
+  res.status(StatusCodes.OK).json(job);
 }
 
-const deleteJob = (req, res) => {
-  res.send("delete a job");
+const deleteJob = async (req, res) => {
+  const createdBy = req.user.userId;
+  const jobID = req.params.id;
+  const job = await Job.findByIdAndDelete({createdBy, _id: jobID});
+  if (!job) throw new NotFound(`The given id: ${jobID} has no job associated with it`);
+  res.status(StatusCodes.OK).send();
 }
 
 module.exports = {
