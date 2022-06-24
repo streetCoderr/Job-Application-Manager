dcb4e50df268b07270f05ef1bd6f6c62049bf798
streetCoder <streetcoder99@gmail.com>
2022-06-24 16:20:02 +0100

    add extra security

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 require('dotenv').config();
 require("express-async-errors");
+const cors = require('cors');
+const helmet = require('helmet');
+const xss_clean = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 const express = require('express');
 const app = express();
+
 const connectDB = require('./db/connect');
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
@@ -11,10 +16,24 @@ const notFound = require("./middlewares/notFound");
 const errorHandler = require("./middlewares/errorHandler");
 
 app.use(express.json());
+// security
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+}))
+app.use(helmet());
+app.use(cors());
+app.use(xss_clean())
 
+
+// routes
 app.use('/api/v1/jobs', authorize, jobsRouter);
 app.use('/api/v1/auth', authRouter);
 
+// middlewares
 app.use(notFound);
 app.use(errorHandler);
 
