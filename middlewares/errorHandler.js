c6bb011fd593ab63d1dd06c9be3639dfdea9518c
streetCoder <streetcoder99@gmail.com>
2022-06-24 15:58:13 +0100

    improve error handling

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,11 +1,26 @@
 const { StatusCodes } = require("http-status-codes")
 
 const errorHandler = (err, req, res, next) => {
-  let error = {
+  let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     message: err.message || "Something went wrong. Try again later"
   }
-  return res.status(error.statusCode).json({msg: error.message});
+
+  if (err.name === 'CastError') {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = `The provided id: ${err.value} is not a valid id`
+  }
+
+  if (err.name === 'ValidationError') {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = Object.values(err.errors).map(val => val.message).join('; ')
+  }
+
+  if (err.code && err.code === 11000) {
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+    customError.message = `Please use another ${Object.keys(err.keyValue)}; ${Object.values(err.keyValue)} has been taken already`
+  }
+  return res.status(customError.statusCode).json({msg: customError.message});
 }
 
 module.exports = errorHandler
